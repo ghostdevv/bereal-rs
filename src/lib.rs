a3106da3fcc76807e2f14ba8b35e390f62945fb0
GHOST <ghostdevbusiness@gmail.com>
2024-04-08 04:11:39 +0100

    feat: add lookup api

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // todo add doc comments
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct Time {
@@ -16,6 +16,7 @@ pub struct Moment {
 }
 
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
 pub struct Regions<T> {
     #[serde(rename = "us-central")]
     us_central: T,
@@ -51,6 +52,57 @@ pub enum Limit {
     All,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum Region {
+    #[serde(rename = "us-central")]
+    USCentral,
+
+    #[serde(rename = "europe-west")]
+    EuropeWest,
+
+    #[serde(rename = "asia-west")]
+    AsiaEast,
+
+    #[serde(rename = "asia-east")]
+    AsiaWest,
+}
+
+impl Region {
+    pub fn to_string(&self) -> String {
+        match self {
+            Region::USCentral => String::from("us-central"),
+            Region::EuropeWest => String::from("europe-west"),
+            Region::AsiaEast => String::from("asia-east"),
+            Region::AsiaWest => String::from("asia-east"),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LookupResultTimeUTC {
+    unix: i64,
+    timestamp: String,
+}
+
+// #[derive(Debug, Deserialize)]
+// pub struct LookupResultTimeLocalized {
+//     // timezone: String,
+//     timestamp: String,
+// }
+
+#[derive(Debug, Deserialize)]
+pub struct LookupResult {
+    region: Region,
+
+    id: String,
+
+    #[serde(rename = "UTC")]
+    utc: LookupResultTimeUTC,
+    // todo this seems broken in api
+    // localized: LookupResultTimeLocalized,
+}
+
 impl BerealClient {
     pub fn new(api_key: &str) -> BerealClient {
         BerealClient {
@@ -68,7 +120,7 @@ impl BerealClient {
             .await
     }
 
-    pub async fn all_moments(&self, limit: Limit) -> Result<AllMoments, reqwest::Error> {
+    pub async fn all_moments(&self, limit: &Limit) -> Result<AllMoments, reqwest::Error> {
         let limit_param = match limit {
             Limit::Count(num) => num.to_string(),
             Limit::All => String::from("NONE"),
@@ -81,6 +133,26 @@ impl BerealClient {
             .send()
             .await?
             .json::<AllMoments>()
+            .await
+    }
+
+    pub async fn lookup(
+        &self,
+        time_zone: &str,
+        date: &str,
+        region: &Region,
+    ) -> Result<LookupResult, reqwest::Error> {
+        reqwest::Client::new()
+            .get("https://bereal.devin.rest/v1/moments/lookup")
+            .query(&[
+                ("api_key", &self.api_key),
+                ("time_zone", &time_zone.to_string()),
+                ("date", &date.to_string()),
+                ("region", &region.to_string()),
+            ])
+            .send()
+            .await?
+            .json::<LookupResult>()
             .await
     }
 }
@@ -105,8 +177,23 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn lookup() {
+        let result = create_client()
+            .lookup("Europe/London", "2024-04-07", &Region::EuropeWest)
+            .await;
+
+        match result {
+            Err(e) => panic!("{:?}", e),
+            Ok(result) => {
+                assert_eq!(result.region.to_string(), Region::EuropeWest.to_string());
+                assert_eq!(result.utc.timestamp, "2024-04-07 11:43:05");
+            }
+        };
+    }
+
+    #[tokio::test]
     async fn all_moments() {
-        match create_client().all_moments(Limit::Count(1)).await {
+        match create_client().all_moments(&Limit::Count(1)).await {
             Err(e) => panic!("{:?}", e),
             Ok(moments) => {
                 if moments.regions.europe_west.len() != 1 {
@@ -118,7 +205,7 @@ mod tests {
 
     #[tokio::test]
     async fn all_moments_limit_default() {
-        match create_client().all_moments(Limit::Default).await {
+        match create_client().all_moments(&Limit::Default).await {
             Err(e) => panic!("{:?}", e),
             Ok(moments) => {
                 let count = moments.regions.europe_west.len();
@@ -132,7 +219,7 @@ mod tests {
 
     #[tokio::test]
     async fn all_moments_limit_all() {
-        match create_client().all_moments(Limit::All).await {
+        match create_client().all_moments(&Limit::All).await {
             Err(e) => panic!("{:?}", e),
             Ok(moments) => {
                 let count = moments.regions.europe_west.len();
