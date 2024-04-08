deadfaa80dfd863378c70b056ed93ed4af71a51a
GHOST <ghostdevbusiness@gmail.com>
2024-04-08 21:34:23 +0100

    feat: add latest moments command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{Local, TimeZone};
 use clap::{Parser, Subcommand};
+use color_eyre::eyre::{eyre, Result};
 
 #[derive(Parser, Debug)]
 #[command(version, about, author)]
@@ -6,22 +8,40 @@ struct Cli {
     #[arg(long, short = 'k', help = "Your bereal.devin.fun api key")]
     api_key: String,
 
-    #[clap(subcommand)]
+    #[command(subcommand)]
     command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Test {
-        #[arg(short, long)]
-        name: String,
-    },
+    /// Get the latest moments from bereal
+    Latest {},
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let cli = Cli::parse();
     let client = bereal::BerealClient::new(&cli.api_key);
 
-    // println!("{}", cli)
+    match cli.command {
+        Commands::Latest {} => {
+            let latest = client.latest_moments().await?;
+
+            println!("Europe:\t\t{}", fmt_date(latest.regions.europe_west.ts)?);
+            println!("America:\t{}", fmt_date(latest.regions.us_central.ts)?);
+            println!("Asia east:\t{}", fmt_date(latest.regions.asia_east.ts)?);
+            println!("Asia west:\t{}", fmt_date(latest.regions.asia_west.ts)?);
+        }
+    }
+
+    Ok(())
+}
+
+fn fmt_date(timestamp: i64) -> Result<String> {
+    let datetime = Local.timestamp_opt(timestamp, 0).single();
+
+    match datetime {
+        Some(ts) => Ok(ts.format("%Y-%m-%d %H:%M:%S").to_string()),
+        None => Err(eyre!("No timestamp found")),
+    }
 }
